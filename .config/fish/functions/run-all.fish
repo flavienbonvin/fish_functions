7d69250c3f7890cb0c9321193d570cf69ff4b743
Flavien Bonvin <flavien.bonvin@pm.me>
2023-07-05 11:41:42 +0200

    Fix command

--- .config/fish/functions/run-all.fish
+++ .config/fish/functions/run-all.fish
@@ -1,5 +1,5 @@
 function run-all -d 'Starting all Proton apps'
     echo 'Starting all Proton apps'
     cd ~/dev/proton/clients
-    yarn start-all --applications "proton-account proton-mail" --api $argv
+    yarn start-all --applications "proton-account proton-mail proton-calendar proton-drive" --api $argv
 end
